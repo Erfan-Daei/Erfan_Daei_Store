1c0410fd54d05be2adfa37c17846c731be1aa31e
Erfan-Daei <135204527+Erfan-Daei@users.noreply.github.com>
2025-11-23 13:34:38 +0330

    Delete Practice_Store.Application/Interfaces/RepositoryManager/Products/IGetCategoriesRepo.cs

--- Practice_Store.Application/Interfaces/RepositoryManager/Products/IGetCategoriesRepo.cs
+++
@@ -1,5 +0,0 @@
-namespace Practice_Store.Application.Interfaces.RepositoryManager.Products.Queries;
-public interface IGetCategoriesRepo
-{
-    List<CategoriesDto>> GetCategories(long? ParentId);
-}
